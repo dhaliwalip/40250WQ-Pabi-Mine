b9b2d1d32832c37c5b617ca169ed57b3df40a8b3
dhaliwalip <dhaliwalip@gmail.com>
2020-02-12 12:02:02 -0800

    DatasbaseService, partial submit

---
+++ Mine/Mine/Services/DatabaseService.cs
@@ -0,0 +1,75 @@
+﻿using SQLite;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Mine.Models;
+using System.Collections.Generic;
+
+namespace Mine.Services
+{
+    /// <summary>
+    /// Database Services
+    /// Will write to the local data store
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DatabaseService<T> : IDataStore<ItemModel> where T : new()
+    {
+        /// <summary>
+        /// Set the class to load on demand
+        /// Saves app boot time
+        /// </summary>
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        static bool initialized = false;
+
+        /// <summary>
+        /// Constructor
+        /// All the database to start up
+        /// </summary>
+        public DatabaseService()
+        {
+            InitializeAsync().SafeFireAndForget(false);
+        }
+
+        /// <summary>
+        /// Create the Table if it does not exist
+        /// </summary>
+        /// <returns></returns>
+        async Task InitializeAsync()
+        {
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(m => m.MappedType.Name == typeof(T).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(T)).ConfigureAwait(false);
+                    initialized = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Wipe Data List
+        /// Drop the tables and create new ones
+        /// </summary>
+        public async Task<bool> WipeDataListAsync()
+        {
+            try
+            {
+                await Database.DropTableAsync<T>().ConfigureAwait(false);
+                await Database.CreateTablesAsync(CreateFlags.None, typeof(T));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error WipeData" + e.Message);
+            }
+
+            return await Task.FromResult(true);
+        }
+  
+       
+    }
+}
